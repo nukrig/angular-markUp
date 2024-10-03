66f9cc48871cf142baad8c7de3d3d2d24461f6be
Nukri Gegelashvili <N.gegelashvili@socar.ge>
2024-10-03 18:12:42 +0400

    created footer page's markup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeroComponent } from "./components/hero/hero.component";
 import { AboutComponent } from "./components/about/about.component";
 import { WorksComponent } from "./components/works/works.component";
 import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HeroComponent, AboutComponent, WorksComponent, TestimonialsComponent],
+  imports: [RouterOutlet, NavBarComponent, HeroComponent, AboutComponent, WorksComponent, TestimonialsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
