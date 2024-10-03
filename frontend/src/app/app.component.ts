7bff1c0862d98613466516e6431e0ac87da7f9e3
Nukri Gegelashvili <N.gegelashvili@socar.ge>
2024-10-03 10:44:50 +0400

    created starter page, added components such as navbar, hero and about component, added responsiveness on this components and created little logic which implements menu button on mobile or other technics under 750px. also the project has smooth scroll on click the navigation list.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { HeroComponent } from "./components/hero/hero.component";
+import { AboutComponent } from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
